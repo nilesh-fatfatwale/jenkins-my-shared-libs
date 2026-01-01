911672b581c37ae4998ae6aa69f1a2ee00d1e44d
Nilesh Fatfatwale <89182735+nilesh-fatfatwale@users.noreply.github.com>
2026-01-01 19:24:20 +0530

    Delete vars/dockerBuild.groovy

--- vars/dockerBuild.groovy
+++
@@ -1,9 +0,0 @@
-def call(String imageName, String imageTag, String context = '.', String dockerfile = 'Dockerfile') {
-    sh """
-        docker build \
-        --no-cache \
-        -f ${dockerfile} \
-        -t ${imageName}:${imageTag} \
-        ${context}
-    """
-}
